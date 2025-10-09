5dfb892c0a080a29b8b059b93ba33dd19e7ed29c
Ariels <ascastro875@gmail.com>
2025-10-09 16:43:59 -0500

    removed 1 proyect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,19 +22,6 @@ function Projects() {
             </div>
           </div>
           <div className="project-card">
-            <img src="https://i.imgur.com/RZLEEXA.png" alt={t('projects.menu.imageAlt')}className="project-image" />
-            <div className="project-card-content">
-              <h3>{t('projects.menu.title')}</h3>
-              <p className="text-muted">
-                {t('projects.menu.description')}
-              </p>
-              <div className="project-links">
-                <a href="https://menu-chifa-amazonas.pages.dev/" className="btn btn-primary">{t('projects.liveSite')}</a>
-                <a href="https://github.com/Ariels875/Restaurant-Menu" className="btn btn-secondary">{t('projects.sourceCode')}</a>
-              </div>
-            </div>
-          </div>
-          <div className="project-card">
             <img src="https://i.imgur.com/Fo03rIM.png" alt={t('projects.mpiq.imageAlt')} className="project-image" />
             <div className="project-card-content">
               <h3>{t('projects.mpiq.title')}</h3>
